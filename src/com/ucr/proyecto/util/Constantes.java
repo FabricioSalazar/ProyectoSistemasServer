9ea3db3761fa11550e05ff73e9f590eadcea2e9b
Usuario <Usuario@192.168.0.129>
2016-10-11 09:47:12 -0600

    Modificacion Constantes Valores BD

--- src/com/ucr/proyecto/util/Constantes.java
+++ src/com/ucr/proyecto/util/Constantes.java
@@ -7,9 +7,9 @@ package com.ucr.proyecto.util;
 
 public class Constantes {
 
-    public static String DB_USER = "Juan";
-    public static String USER_PASS = "123456";
-    public static String DB_URL = "jdbc:sqlserver://Juan:1433;databaseName=Encuesta";
+    public static String DB_USER = "sqlserver";
+    public static String USER_PASS = "saucr.12";
+    public static String DB_URL = "jdbc:sqlserver://163.178.107.130;databaseName=Proyecto_II_Operativos_B32144_B56535_B44540_B56246";
     public static String SQL_SERVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 
 }
