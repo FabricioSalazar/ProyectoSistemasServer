e457067ea9d13a0f266756b0912ce79fdf162218
JuanCarlos <JuanCarlos@Juan>
2016-10-16 20:19:07 -0600

    Q

--- src/com/ucr/proyecto/util/Constantes.java
+++ src/com/ucr/proyecto/util/Constantes.java
@@ -13,5 +13,11 @@ public class Constantes {
     public static String USER_PASS = "saucr.12";
     public static String DB_URL = "jdbc:sqlserver://163.178.107.130;databaseName=Proyecto_II_Operativos_B32144_B56535_B44540_B56246";
     public static String SQL_SERVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static Empleado empleadoNulo=new Empleado("No suministrado", "null", 0, "00000000", "No suministrado", 0);
+    public static Empleado empleadoNulo = new Empleado("No suministrado", "null", 0, "00000000", "No suministrado", 0);
+    
+    public static final String ENVIAR_TRANSACCION = "ENVIAR";
+    public static final String ACTUALIZACION_DE_ESTADOO = "NUEVO_ESTADO";
+    public static final String VERIFICACION_DE_DATOS = "VERIFICAR_DATOS";
+    public static final String WAIT_CLIENT = "EN_ESPERA";
+
 }
